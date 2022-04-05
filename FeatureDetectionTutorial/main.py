c51fbd7ae1a463a80eaacd706a0cb6e2d0edbe0a
OVERPOWERED99 <jasonsutardi@gmail.com>
2022-04-05 19:43:06 +0800

    Update main.py

---
+++ FeatureDetectionTutorial/main.py
@@ -0,0 +1,66 @@
+import cv2
+import os
+
+path = 'Documents'
+images = []
+documentTypes = []
+files = os.listdir('Documents')
+orb = cv2.ORB_create(nfeatures=1000)
+print('Total Documents Detected:', len(files))
+
+# Import images and document types
+for file in files:
+    currentImg = cv2.imread(f'{path}/{file}', 0)
+    images.append(currentImg)
+    documentTypes.append(os.path.splitext(file)[0])
+
+
+def findDes(images):
+    desList = []
+    for img in images:
+        kp, des = orb.detectAndCompute(img, None)
+        desList.append(des)
+    return desList
+
+
+def findID(img, desList, threshold):
+    kp2, des2 = orb.detectAndCompute(img, None)
+    bf = cv2.BFMatcher()
+    matchList = []
+    finalID = -1
+    try:
+        for des in desList:
+            matches = bf.knnMatch(des, des2, k=2)
+            good = []
+            for m, n in matches:
+                if m.distance < 0.75 * n.distance:
+                    good.append([m])
+            matchList.append(len(good))
+    except:
+        pass
+
+    print(matchList)
+
+    if len(matchList) != 0:
+        if max(matchList) > threshold:
+            finalID = matchList.index(max(matchList))
+    return finalID
+
+
+desList = findDes(images)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img2 = cap.read()
+    imgOriginal = img2.copy()
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    id = findID(img2, desList, 10)
+    if id >= 0:
+        cv2.putText(imgOriginal, documentTypes[id], (50, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))
+    else:
+        cv2.putText(imgOriginal, 'No document detected', (50, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))
+
+    cv2.imshow('img2', imgOriginal)
+    cv2.waitKey(1)
